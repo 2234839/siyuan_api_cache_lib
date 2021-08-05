6d2c1ab39b9b71f988eb7bd4eaf9700317763d51
崮生 <admin@shenzilong.cn>
2021-08-05 20:24:50 +0800

    ✨ feat(attr): 新增查询单个属性的 api

--- src/siyuan-api/attr.ts
+++ src/siyuan-api/attr.ts
@@ -27,16 +27,25 @@ export namespace setBlockAttrs {
 }
 /**
  * 查询块属性
- *
+ * 在查询自身时该怎么 cache ，如果直接  cache 会导致无限增长
  * tips: 可以使用 {@link htmlDecode util.htmlDecode} 对 {@link attrRow} 的 value 进行解码
  */
-export async function getBlockAttrs(id: string): Promise<{
-  code: number;
-  msg: string;
-  data: attrRow[];
-}> {
-  return await sqlQuery(`SELECT * FROM "attributes" WHERE block_id='${id}'`);
+export async function getBlockAttrs(id: string) {
+  const r = sqlQuery<attrRow[]>(
+    `SELECT * FROM "attributes" WHERE block_id='${id}'`
+  );
+
+  return r;
 }
 export namespace getBlockAttrs {
+  export let noCache = false;
+}
+
+/** 查询单个属性 */
+export async function getBlockAttr(id: string, name: string) {
+  const attrs = (await getBlockAttrs(id)).data;
+  return attrs.find((el) => el.name === name);
+}
+export namespace getBlockAttr {
   export let noCache = false;
 }
