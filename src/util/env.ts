96426f1cd600d18ada8c5896790dbd9df59d4bbb
崮生 <admin@shenzilong.cn>
2021-09-05 20:25:30 +0800

    🐞 fix(env.ts): 非同源环境不直接报错

--- src/util/env.ts
+++ src/util/env.ts
@@ -1,4 +1,10 @@
 export function getCurrentEnv(): getCurrentEnv.env {
+  try {
+    self.parent?.document;
+  } catch (e) {
+    // 处于非同源 iframe，无法探知当前环境
+    return getCurrentEnv.env.unknown;
+  }
   if (querySelector(`main[data-n-id]`)) {
     return getCurrentEnv.env.oceanPress;
   } else if (
